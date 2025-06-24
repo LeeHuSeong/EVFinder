314016a24327d951e950db5dc0beeafecbdb748e
LeeHuSeong <dlgntjdsla@gmail.com>
2025-06-24 20:41:52 +0900

    start

--- evfinder/lib/main.dart
+++ evfinder/lib/main.dart
@@ -1,64 +1,45 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await _initialize();
+  runApp(const NaverMapApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// 지도 초기화하기
+Future<void> _initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(
+    clientId: 'qe05hz13nm',     // 클라이언트 ID 설정
+    onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed")
+  );
+}
+
+class NaverMapApp extends StatelessWidget {
+  const NaverMapApp({Key? key});
+
   @override
   Widget build(BuildContext context) {
+    // NaverMapController 객체의 비동기 작업 완료를 나타내는 Completer 생성
+    final Completer<NaverMapController> mapControllerCompleter = Completer();
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        body: NaverMap(
+          options: const NaverMapViewOptions(
+            indoorEnable: true,             // 실내 맵 사용 가능 여부 설정
+            locationButtonEnable: false,    // 위치 버튼 표시 여부 설정
+            consumeSymbolTapEvents: false,  // 심볼 탭 이벤트 소비 여부 설정
+          ),
+          onMapReady: (controller) async {                // 지도 준비 완료 시 호출되는 콜백 함수
+            mapControllerCompleter.complete(controller);  // Completer에 지도 컨트롤러 완료 신호 전송
+            log("onMapReady", name: "onMapReady");
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }

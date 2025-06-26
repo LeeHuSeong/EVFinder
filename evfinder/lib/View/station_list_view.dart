b71cc233058ce0c3760add2cbe60ef3a26ab96e3
LeeHuSeong <dlgntjdsla@gmail.com>
2025-06-26 12:53:11 +0900

    FireStore연결 성공

---
+++ evfinder/lib/View/station_list_view.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import '../Controller/station_controller.dart';
+import '../Model/station_model.dart';
+
+class StationListView extends StatefulWidget {
+  const StationListView({super.key});
+
+  @override
+  State<StationListView> createState() => _StationListViewState();
+}
+
+class _StationListViewState extends State<StationListView> {
+  final StationController _controller = StationController();
+  List<ChargingStation> _stations = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadStations();
+  }
+
+  Future<void> _loadStations() async {
+    final stations = await _controller.fetchStations();
+    setState(() {
+      _stations = stations;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('충전소 목록')),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _stations.length,
+              itemBuilder: (context, index) {
+                final station = _stations[index];
+                return ListTile(
+                  title: Text(station.name),
+                  subtitle: Text(
+                    '${station.location.latitude}, ${station.location.longitude}',
+                  ),
+                  onTap: () {
+                    // 충전소 상세 이동 or chargers 조회
+                  },
+                );
+              },
+            ),
+    );
+  }
+}
--- evfinder/lib/main.dart
+++ evfinder/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'View/login_view.dart';  // 로그인 화면 import
+import 'View/station_list_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); // Firebase 초기화
-  
+
   runApp(const MyApp());
 }
 
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const LoginView(),  // 로그인 화면이 처음 뜨도록
+      home: const StationListView()
+      //home: const LoginView(),  // 로그인 화면이 처음 뜨도록
     );
   }
 }

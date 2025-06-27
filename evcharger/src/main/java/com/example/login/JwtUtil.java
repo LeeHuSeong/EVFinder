4fe336a02eff2f3d92a13cd354b3841e7f91b598
LeeKyungBum <lkb3311@gmail.com>
2025-06-27 19:54:47 +0900

    로그인(jwt토큰 검증)기능 추가

---
+++ evcharger/src/main/java/com/example/login/JwtUtil.java
@@ -0,0 +1,22 @@
+package com.example.login;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
+
+    public static String generateToken(String uid, String email) {
+        return Jwts.builder()
+                .setSubject(uid)
+                .claim("email", email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+}

f7bc8625db164a7085cceb4c47fee12acfffd895
yhw <guddn50521@gmail.com>
2025-06-27 17:54:04 +0900

    메인화면 충전소 검색 Tap하면 화면 이동, 충전소 찾는 화면에 리스트뷰 추가

--- evfinder/lib/View/search_charger_view.dart
+++ evfinder/lib/View/search_charger_view.dart
@@ -12,30 +12,47 @@ class _SearchChargerViewState extends State<SearchChargerView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Container(
-            height: MediaQuery.of(context).size.height * 0.06,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: Colors.white,
-              border: Border.all(color: Colors.grey),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.06,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                ),
+                child: ListTile(
+                  leading: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.arrow_back),
+                  ),
+                  trailing: Icon(Icons.search),
+                  title: TextField(
+                    // onSubmitted: submitted,
+                    // controller: controller,
+                    decoration: InputDecoration(hintText: "충전소 검색", border: InputBorder.none),
+                  ),
+                ),
+              ),
             ),
-            child: ListTile(
-              leading: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.82,
+              child: ListView.separated(
+                itemCount: 100,
+                // primary: false,
+                physics: AlwaysScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Text(index.toString());
                 },
-                icon: Icon(Icons.arrow_back),
-              ),
-              trailing: Icon(Icons.search),
-              title: TextField(
-                // onSubmitted: submitted,
-                // controller: controller,
-                decoration: InputDecoration(hintText: "충전소 검색", border: InputBorder.none),
+                separatorBuilder: (BuildContext context, int index) => Divider(),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );

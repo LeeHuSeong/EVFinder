6fcc361dbc4bfdc607f85a086dfb0d5b908558c6
eunji56 <mefdllg2g35@gmail.com>
2025-07-22 09:30:18 +0900

    비밀번호 변경

--- evfinder/lib/View/profile_view.dart
+++ evfinder/lib/View/profile_view.dart
@@ -1,9 +1,26 @@
 import 'package:evfinder/View/widget/profile_card.dart';
 import 'package:flutter/material.dart';
+import '../Controller/login_controller.dart';
+import '../View/change_password_view.dart';
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
 
+
+@override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+class _ProfileViewState extends State<ProfileView> {
+  final LoginController _controller = LoginController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  bool isLoading = false;
+
+  void handleChangePassword() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const ChangePasswordView()),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -15,13 +32,26 @@ class ProfileView extends StatelessWidget {
           SingleChildScrollView(
             child: Column(
               children: [
-                // Padding(
-                //   padding: const EdgeInsets.only(left: 20.0, top: 15),
-                //   child: ListTile(
-                //     leading: Icon(Icons.star, color: Colors.yellow),
-                //     title: Text("즐겨찾기"),
-                //   ),
-                // ),
+                const SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: ElevatedButton(
+                    onPressed: handleChangePassword,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF3B82F6), // blue-500
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                      elevation: 0,
+                    ),
+                    child: const Text(
+                      '비밀번호 변경',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+               
               ],
             ),
           ),
